7a8d4a52d554e21cc354252f9136e0b96650456a
tuure <lukasz.witkowski@allegro.pl>
2021-12-13 11:18:39 +0000

    new

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -67,23 +67,22 @@ public class Application {
     PlayerState me = arenaUpdate.arena.state.getOrDefault(href, null);
     int max = 0;
     licznik++;
-    return "T";
-    // if(me.wasHit && !(!ruch || mypositiony.equals( me.y) && mypositionx.equals(me.x))) {
-    //     ruch = true;
-    //     return "F";
-    // }
-    // ruch = false;
-    // mypositionx = me.x;
-    // mypositiony = me.y;
-    // if(licznik %2 ==0){
-    //     System.out.println(me.x+ " "+me.y+ "L");
-    //     return "L";
-    // }
-    // else {
-    //   System.out.println(me.x+ " "+me.y+ "T");
-    //   if(hitSomeone(me, arenaUpdate)) licznik++;
-    //   return "T";
-    // }
+    if(me.wasHit && (!ruch || !(mypositiony == me.y && mypositionx == me.x))) {
+        ruch = true;
+        return "F";
+    }
+    ruch = false;
+    mypositionx = me.x;
+    mypositiony = me.y;
+    if(licznik %2 ==0){
+        System.out.println(me.x+ " "+me.y+ "L");
+        return "L";
+    }
+    else {
+      System.out.println(me.x+ " "+me.y+ "T");
+      if(hitSomeone(me, arenaUpdate)) licznik++;
+      return "T";
+    }
   }
 
     private boolean hitSomeone(PlayerState me, ArenaUpdate arenaUpdate) {
